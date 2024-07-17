ccd4b634c1e8c31360d0a90c3d5303a6b0a4dd47
Felix Konrad <110739309+fekonrad@users.noreply.github.com>
2024-07-17 09:10:47 +0200

    Added additional Test for COptLayer.

--- Tests/test_COptLayer.py
+++ Tests/test_COptLayer.py
@@ -10,17 +10,30 @@ We perform the following tests for the COptLayer:
         Therefore the expected value of theta^T (f(theta + Z) Z^T) should be all 0 (?)
 """
 def test_gradient():
+    d = 2
+    theta = torch.ones((1, d), dtype=torch.float32, requires_grad=True)
+    layer = COptLayer(solver=solver_Simplex,
+                      num_samples=10000,
+                      smoothing=1.0)
+    sol = layer(theta)
+    loss = (torch.ones((1, d)) * sol).sum()
+    loss.backward()
+    print(f"theta.shape == {theta.shape}")
+    print(f"theta.grad == {theta.grad}")
+    if torch.norm(theta.grad) > 1e-2:
+        raise Exception(f"Gradient should be equal to 0 (for theta=[1.0, 1.0] and theta_opt=[1.0, 1.0]) \n theta.grad == {theta.grad.detach().numpy()}")
+
     theta = torch.tensor([[1.0, 1.0]], dtype=torch.float32, requires_grad=True)
     layer = COptLayer(solver=solver_Simplex,
                       num_samples=10000,
                       smoothing=1.0)
     sol = layer(theta)
-    loss = (theta * sol).sum()
+    loss = (torch.tensor([1.5, 0.5]) * sol).sum()
     loss.backward()
     print(f"theta.shape == {theta.shape}")
     print(f"theta.grad == {theta.grad}")
-    if torch.norm(theta.grad[0, 0] - theta.grad[0, 1]) > 1e-3:
-        raise Exception(f"Entries of Gradient w.r.t. theta=[1.0, 1.0] are not equal. \n theta.grad == {theta.grad.detach().numpy()}")
+    if theta.grad[0, 0] < theta.grad[0, 1]:
+        raise Exception(f"First Component of Gradient should be greater than second component (for theta=[1.0, 1.0] and theta_opt=[1.5, 0.5]) \n theta.grad == {theta.grad.detach().numpy()}")
 
 
 if __name__ == "__main__":
