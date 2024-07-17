c8f0885a05198e0e839ae61b5438a878fbaa843b
Felix Konrad <110739309+fekonrad@users.noreply.github.com>
2024-07-17 17:13:41 +0200

    Fixed Perturbed Loss Function
    Changed shape of `params` to be compatible with `solver`.

--- losses.py
+++ losses.py
@@ -1,5 +1,4 @@
 from typing import Any
-
 import torch
 from torch import nn
 
@@ -15,26 +14,34 @@ class PerturbedLossFct(torch.autograd.Function):
                 smoothing: float
                 ) -> Any:
         z = torch.rand((num_samples, *theta.shape))
-        params = theta + smoothing * z
+        params = theta + smoothing * z          # shape (n, b, h, w)
+        p_shape = params.shape
+        n, b = p_shape[0], p_shape[1]
+        params = params.view(n*b, *p_shape[2:])         # shape (n*b, h, w)
 
         ctx.y, ctx.objective, ctx.solver, ctx.params = (
                 y, objective, solver, params)               # store for backward function
+        ctx.n, ctx.b, = n, b
 
         if objective == 'min':
-            return (torch.dot(y.flatten(), theta.flatten()) -
-                    torch.bmm(ctx.solver(params).view(num_samples, 1, -1), params.view(num_samples, -1, 1)).mean())
+            return (y.unsqueeze(0) * params.view(n, b, *p_shape[2:]) -
+                    ctx.solver(params).view(n, b, *p_shape[2:]) * params.view(n, b, *p_shape[2:])).mean(dim=0).sum()
 
-        return (torch.bmm(ctx.solver(params).view(num_samples, 1, -1), params.view(num_samples, -1, 1)).mean() -
-                torch.dot(y.flatten(), theta.flatten()))
+        return (ctx.solver(params).view(n, b, *p_shape[2:]) * params.view(n, b, *p_shape[2:]) -
+                y.unsqueeze(0) * params.view(n, b, *p_shape[2:])).mean(dim=0).sum()
 
     @staticmethod
     def backward(ctx: Any, *grad_outputs: Any) -> Any:
         grad_output_tensor = grad_outputs[0]
         grad_output_tensor = grad_output_tensor.unsqueeze(-1)
+        params = ctx.params
+        p_shape = params.shape
+        n, b = ctx.n, ctx.b
 
         if ctx.objective == 'min':
-            return (ctx.y - (ctx.solver(ctx.params).mean(dim=0))) * grad_output_tensor, None, None, None, None, None
-        return (ctx.solver(ctx.params).mean(dim=0) - ctx.y) * grad_output_tensor, None, None, None, None, None
+            return (ctx.y - (ctx.solver(ctx.params).view(n, b, *p_shape[1:]).mean(dim=0))) * grad_output_tensor, None, None, None, None, None
+
+        return (ctx.solver(ctx.params).view(n, b, *p_shape[1:]).mean(dim=0) - ctx.y) * grad_output_tensor, None, None, None, None, None
 
 
 class PerturbedLoss(nn.Module):
@@ -49,17 +56,17 @@ class PerturbedLoss(nn.Module):
         """
         super().__init__()
         self.solver = solver
-        self.objective = objective
+        self.objective = objective.lower()
         self.num_samples = num_samples
         self.smoothing = smoothing
 
-        if not (objective == 'min' or objective == 'max'):
+        if not (self.objective == 'min' or self.objective == 'max'):
             raise KeyError("Keyword 'objective' either has to be 'min' (default) or 'max'.")
 
     def forward(self, theta, y):
         """
-        :param theta:
-        :param y:
+        :param theta: torch.tensor of shape (b, *input_dim); `solver` needs to be able to process tensors of such shape
+        :param y: torch.tensor of shape (input_dim) representing (approximate) optimal solution.
         :return:
         """
         return PerturbedLossFct.apply(theta,
@@ -74,7 +81,7 @@ def solver_Simplex(theta: torch.tensor):
     max_ind = torch.argmax(theta, dim=1)
     sol = torch.zeros_like(theta)
     for i, ind in enumerate(max_ind):
-        sol[i, ind[0]] = 1.0
+        sol[i, ind.item()] = 1.0
     return sol
 
 
@@ -84,12 +91,16 @@ if __name__ == "__main__":
     solver = solver_Simplex
 
     loss_fn = PerturbedLoss(solver,
-                            objective='min',
-                            num_samples=100,
+                            objective='max',
+                            num_samples=10,
                             smoothing=1.0)
 
-    y = torch.tensor([[1.0, 1.0]], dtype=torch.float32)
+    y = torch.tensor([[0.0, 1.0]], dtype=torch.float32)
     loss = loss_fn(theta, y)
     print(f"Loss: {loss.item()}")
     loss.backward()
-    print(f"Gradient: {theta.grad}")
+    print(f"Gradient: {theta.grad.detach().numpy()}")
+    """
+        I don't think this example works? 
+        The gradient here is of the form [a, -a] for a>0. But shouldn't it be the other way around?
+    """
